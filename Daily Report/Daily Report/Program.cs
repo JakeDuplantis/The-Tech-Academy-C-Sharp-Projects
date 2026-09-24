59a88e8a0b7b4543cf81eff42e405673cdd76823
Jacob Duplantis <duplantisjacob54@gmail.com>
2026-09-24 08:04:38 -0500

    Added Daily Report
    Daily report folder contains a console app that asks questions and saves variable to their proper data type.

---
+++ Daily Report/Daily Report/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_Report
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What is your name?");
+            string studentName = Console.ReadLine();
+
+            Console.WriteLine("What Course are you on?");
+            string courseName = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Do You need help with anything? Please answer \"true\" or \"false\". ");
+            string needHelp = Console.ReadLine();
+            bool result = bool.Parse(needHelp);
+           
+
+            Console.WriteLine("Were there any positive experiences you'd like to provide? Please give specifics.");
+            string experience = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            int studyHours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
