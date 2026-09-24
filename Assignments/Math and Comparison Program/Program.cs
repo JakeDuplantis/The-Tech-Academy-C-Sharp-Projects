3b7caf82410bcc4cc42abd992078448d8ae890d8
Jacob Duplantis <duplantisjacob54@gmail.com>
2026-09-24 15:15:55 -0500

    Added anonymous income comparison
    anonymous income comparison contains a program to take rate an hours worked anonymously and compare annual salaries

---
+++ Assignments/Math and Comparison Program/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymous_income_comparison_program
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anomymous income Comparison Program");
+
+            //person1
+            Console.WriteLine("Person1");
+            Console.WriteLine("Hourly rate?");
+            string p1_Rate = Console.ReadLine();
+            Convert.ToInt32(p1_Rate);
+            Console.WriteLine("Person1 rate is " + p1_Rate);
+
+            Console.WriteLine("Hours worked per week?");
+            string p1_HoursWorked = Console.ReadLine();
+            Convert.ToInt32(p1_HoursWorked);
+            Console.WriteLine("Person1 worked " + p1_HoursWorked + " hours");
+
+            //person2
+            Console.WriteLine("Person2");
+            Console.WriteLine("Hourly rate?");
+            string p2_Rate = Console.ReadLine();
+            Console.WriteLine("Person2 rate is " + p2_Rate);
+
+            Console.WriteLine("Hours worked per week?");
+            string p2_HoursWorked = Console.ReadLine();
+            Console.WriteLine("Person2 worked " + p2_HoursWorked + " hours");
+
+            Console.WriteLine("Annual Salary of Person 1:");
+            int Annualsalary1 = Convert.ToInt32(p1_Rate) * Convert.ToInt32(p1_HoursWorked) * 52;
+            Console.WriteLine(Annualsalary1);
+
+            Console.WriteLine("Annual Salary of Person 2:");
+            int Annualsalary2 = Convert.ToInt32(p2_Rate) * Convert.ToInt32(p2_HoursWorked) * 52;
+            Console.WriteLine(Annualsalary2);
+
+            Console.WriteLine("Does Person 1 make more money than person 2?");
+            bool salCheck = Annualsalary1 > Annualsalary2;
+            Console.WriteLine(salCheck);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignments/Math and Comparison Program/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Math and Comparison Program")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Math and Comparison Program")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96c6ebc9-1d93-4a22-909a-1abd3b3442d0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
